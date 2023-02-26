7ea9dba1ee0b9f0a4d64a9d22097d89d7ad50a38
marfushhaaa <p1m455w96@gmail.com>
2023-02-27 00:00:11 +0100

    feat: create addChallenge screen
    I made a "CreateChallengeScreen"

---
+++ app/src/main/java/socialmedia/prosperity/newecosocialmedia/CreateChallengeActivity.java
@@ -0,0 +1,14 @@
+package socialmedia.prosperity.newecosocialmedia;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class CreateChallengeActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.challenge_add_screen);
+    }
+}
